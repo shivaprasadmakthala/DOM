0b2c3d0ee19b06718d5a7defa872211ba21c93a2
shivaprasadmakthala <91283153+shivaprasadmakthala@users.noreply.github.com>
2022-03-22 17:25:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,105 @@
+"use strict";
+// console.log("Suman Gangopadyay");
+
+// document.write("I live in Bangalore");
+
+// let data = {name:"suman", location:"bangalore"};
+// for(let key in data){
+//     console.log(key, data[key]);
+// }
+
+// use JS to find the number is eve or odd 
+
+// let num1 = 10;
+// if(num1 %2 == 0){
+//     console.log("even");
+// }else {
+//     console.log("odd");
+// }
+
+
+// let name = "tanushree";
+
+// if(name == "suman"){
+//     console.log("mentor");
+// }else if(name == "tanushree"){
+//     console.log("project manager");
+// } else if(name == "nithin"){
+//     console.log("director");
+// } else {
+//     console.log("Outsider");
+// }
+
+////////////////////////////////////////////////////////////////
+
+
+
+
+
+// //innerHtml DOM
+// //Documnet object manipulation
+// const tags = document.getElementsByTagName('p');
+// const suman = document.getElementById('suman');
+
+// //catch all the <p> and concatenate inside a div element
+// suman.innerHTML = tags[0].innerHTML + tags[1].innerHTML;
+
+
+//change the image using DOM to pizza
+
+// const suman = document.getElementById('first');
+// suman.src = '';
+
+
+////////////////////
+
+//DOM with HTML forms
+
+// function Suman(){
+//     const form_data = document.forms["user_form"]["first_name"].value;
+//     document.getElementById("display").innerHTML = form_data;
+// }
+
+////////////////////////////////
+
+// const suman = document.createElement("p");
+
+// const data = document.createTextNode("THIS is Suman paragraph");
+
+// const final = document.getElementById("suman");
+
+// suman.appendChild(data);
+// final.appendChild(suman);
+
+////////////////////////////////
+
+
+// DOM
+// Create HTML elements using DOM
+
+// // DIV 1
+// const alpha = document.createElement("div");
+// alpha.id = "suman";
+// alpha.innerHTML = "This is  a DOM Manipulation";
+// document.body.appendChild(alpha);
+
+// //DIV 2
+// const beta = document.createElement("div");
+// beta.id = "ganguly";
+// beta.className = "common";
+// beta.innerHTML = "this is a beta div";
+// // add inline css using JS 
+// // beta.style.backgroundColor = 'black';
+// // beta.style.color = 'yellow';
+// // beta.style.fontFamily = 'cursive';
+// // beta.style.alignContent = 'center';
+// document.body.appendChild(beta);
+
+// //DELETE, REMOVE a HTML Element using Manipulation
+
+// document.getElementById('btn1').addEventListener("click", function(){
+//     document.getElementById("suman").remove();//remove that HTML element whose id is Suman
+// });
+
+
+
